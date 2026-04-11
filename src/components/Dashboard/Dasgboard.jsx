2bf8bbe860907f51fd46fa92e352c87ae0334d68
taniashahida-dev <taniashahida993@gmail.com>
2026-04-11 21:15:34 +0600

    added pie-chart

--- src/components/Dashboard/Dasgboard.jsx
+++ src/components/Dashboard/Dasgboard.jsx
@@ -1,10 +1,47 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { Legend, Pie, PieChart, Tooltip } from 'recharts';
+import { AppProvider } from '../../appContext/AppContext';
+import useApps from '../../hooks/useApps';
 
 const Dasgboard = () => {
+    const {installApps} = useContext(AppProvider)
+     const {apps }= useApps()
+     const uninstallApps = apps.length - installApps.length
+
+const data = [
+     
+  { name: 'Installed-Apps', value: installApps.length , fill: '#0088FE' },
+  { name: 'Uninstalled-Apps', value: uninstallApps, fill: '#FF8042' },
+];
+  
+
+
     return (
-        <div>
-            
+         <div className=" my-10 shadow p-10 rounded-md border border-slate-300 container mx-auto">
+      <h2 className="font-semibold text-3xl mb-16 text-center">
+        Installed and uninstalled apps
+      </h2>
+            <PieChart
+             style={{ width: '100%', maxWidth: '500px', maxHeight: '80vh',  margin: "auto", aspectRatio: 1 }} responsive>
+      <Pie
+        data={data}
+        innerRadius="80%"
+        outerRadius="100%"
+        // Corner radius is the rounded edge of each pie slice
+        cornerRadius="50%"
+        fill="#8884d8"
+        // padding angle is the gap between each pie slice
+        paddingAngle={5}
+        dataKey="value"
+     
+      />
+      <Legend/>
+    <Tooltip/>
+    </PieChart>
+   
+     
         </div>
+         
     );
 };
 
